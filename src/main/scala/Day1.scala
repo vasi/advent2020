6b96276536dc402a6df42b91e7a5f4a990cfc538
Dave Vasilevsky <dave@vasilevsky.ca>
2020-12-01 02:19:35 -0500

    day1-2

--- src/main/scala/Day1.scala
+++ src/main/scala/Day1.scala
@@ -1,10 +1,12 @@
 import scala.io.Source
 
 object Day1 extends App {
+  val Array(file, combinationCount) = args
+
   val ints = Source.fromFile(args.head).getLines.map(_.toInt).toArray
-  val found = ints.combinations(2).flatMap {
-    case Array(i, j) if i + j == 2020 => Some(i * j)
-    case _ => None
+  val found = ints.combinations(combinationCount.toInt).flatMap { vals =>
+    if (vals.sum == 2020) Some(vals.product)
+    else None
   }
   println(found.toList)
 }
