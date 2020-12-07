8c8edd74cb139ca074905a18068a34c29c80d11f
Dave Vasilevsky <dave@vasilevsky.ca>
2020-12-07 00:52:37 -0500

    day7-1

---
+++ src/main/scala/Day7.scala
@@ -0,0 +1,41 @@
+import scala.io.Source
+import scala.collection.mutable
+
+case class BagSpec(color: String, count: Int)
+case class Rule(outerColor: String, inner: Seq[BagSpec])
+
+object Rule {
+  final val RuleRe = """\A(.*) bags contain (.*)\.\z""".r
+  final val InnerRe = """(\d+) (.*) bag(?:s?)""".r
+  final val InnerNone = "no other bags"
+
+  def apply(line: String): Rule = line match {
+    case RuleRe(outer, inner) =>
+      val inners = inner.split(", ").flatMap {
+        case InnerNone => None
+        case InnerRe(num, color) => Some(BagSpec(color, num.toInt))
+      }
+      Rule(outer, inners)
+  }
+}
+
+object Day7 extends App {
+  def canHold(rules: Seq[Rule], color: String): Set[String] = {
+    val colorsSeen = mutable.Set(color)
+    var stop = false
+    var unusedRules = rules
+    while (!stop) {
+      val (used, unused) = unusedRules.partition(_.inner.exists(s => colorsSeen.contains(s.color)))
+      if (used.isEmpty) {
+        stop = true
+      } else {
+        used.foreach(r => colorsSeen.add(r.outerColor))
+        unusedRules = unused
+      }
+    }
+    colorsSeen.toSet - color
+  }
+
+  val rules = Source.fromFile(args.head).getLines.map(Rule.apply).toSeq
+  println(canHold(rules, "shiny gold").size)
+}
