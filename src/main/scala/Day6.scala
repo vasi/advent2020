9ce3fecd50a48f94947b6f32a4877d9916c5f8c8
Dave Vasilevsky <dave@vasilevsky.ca>
2020-12-06 21:44:34 -0500

    day6-2

--- src/main/scala/Day6.scala
+++ src/main/scala/Day6.scala
@@ -2,6 +2,8 @@ import scala.io.Source
 
 object Day6 extends App {
   val groups: Array[Array[String]] = Util.splitLines(Source.fromFile(args.head).getLines())
-  val choices = groups.map { g => g.map(_.chars.toArray.toSet).reduce(_ ++ _).size }.sum
-  println(choices)
+  val sets = groups.map { g => g.map(_.chars.toArray.toSet) }
+  val anyPerGroup = sets.map { g => g.reduce(_ ++ _).size }.sum
+  val allPerGroup = sets.map { g => g.reduce(_ & _ ).size }.sum
+  println(allPerGroup)
 }
