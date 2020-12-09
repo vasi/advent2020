cfc323e500dcf217cd2f13bbc9acab92fdea3ef2
Dave Vasilevsky <dave@vasilevsky.ca>
2020-12-09 05:31:32 -0500

    day9-2

--- src/main/scala/Day9.scala
+++ src/main/scala/Day9.scala
@@ -1,21 +1,42 @@
 import scala.collection.mutable
+import scala.collection.Seq
 import scala.io.Source
 
 object Day9 extends App {
-  def validNum(buf: collection.Seq[Long], n: Long): Boolean = {
+  def validNum(buf: Seq[Long], n: Long): Boolean = {
     buf.combinations(2).exists { case collection.Seq(a, b) =>
       a != b && a + b == n
     }
   }
 
-  val nums = Source.fromFile(args.head).getLines.map(_.toLong)
-  var buffer = mutable.ArrayDeque.empty[Long]
-  for (n <- nums) {
-    if (buffer.length >= 25) {
-      if (!validNum(buffer, n))
-        println(n)
-      buffer.dropInPlace(1)
+  def invalid(buf: Seq[Long]): Option[Long] = {
+    val buffer = mutable.ArrayDeque.empty[Long]
+    buf.foreach { n =>
+      if (buffer.length >= 25) {
+        if (!validNum(buffer, n))
+          return Some(n)
+        buffer.dropInPlace(1)
+      }
+      buffer.append(n)
     }
-    buffer.append(n)
+    None
   }
+
+  def contiguousSum(buf: Seq[Long], sum: Long): Option[Seq[Long]] = {
+    val sums = mutable.ArrayBuffer.empty[Long]
+    buf.indices.foreach { i =>
+      0.until(i).foreach { j =>
+        sums(j) += buf(i)
+        if (sums(j) == sum) return Some(buf.slice(j, i + 1))
+      }
+      sums.append(buf(i))
+    }
+    None
+  }
+
+  val nums = Source.fromFile(args.head).getLines.map(_.toLong).toSeq
+  val inv = invalid(nums).get
+  println(inv)
+  val contig = contiguousSum(nums, inv).get
+  println(contig.min + contig.max)
 }
