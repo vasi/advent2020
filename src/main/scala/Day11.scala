5a4e615e28c11747abb1646e96a77585700e1139
Dave Vasilevsky <dave@vasilevsky.ca>
2020-12-11 00:36:01 -0500

    day11-2

--- src/main/scala/Day11.scala
+++ src/main/scala/Day11.scala
@@ -5,25 +5,21 @@ object Day11 extends App {
     type Position = Value
     val Empty, Occupied, Floor = Value
   }
+  type AdjFunc = (Ferry, Int, Int) => Seq[(Int, Int)]
   case class Ferry(state: Seq[Seq[Position.Value]]) {
     def apply(x: Int, y: Int): Position.Value = state(y)(x)
-    def nearbyIdxs(x: Int, y: Int): Seq[(Int, Int)] = {
-      val all = for {
-        dx <- Seq(-1, 0, 1)
-        dy <- Seq(-1, 0, 1)
-        if dx != 0 || dy != 0
-      } yield (x + dx, y + dy)
-      all.filter { case (xx, yy) => xx >= 0 && xx < state.head.length && yy >= 0 && yy < state.length}
-    }
-    def nearbyOccupied(x: Int, y: Int): Int = nearbyIdxs(x, y).map { case (x, y) => apply(x, y) }
-      .count(_ == Position.Occupied)
+    def valid(x: Int, y: Int): Boolean = x >= 0 && x < state.head.length && y >= 0 && y < state.length
 
-    def next: Ferry = {
+    def nearbyOccupied(x: Int, y: Int, f: AdjFunc): Int = f(this, x, y).count {
+      case(nx, ny) => valid(nx, ny) && apply(nx, ny) == Position.Occupied
+    }
+
+    def next(adjFunc: AdjFunc, threshold: Int): Ferry = {
       val nextState = state.indices.map { y =>
         state(y).indices.map { x =>
-          (apply(x, y), nearbyOccupied(x, y)) match {
+          (apply(x, y), nearbyOccupied(x, y, adjFunc)) match {
             case (Position.Empty, o) if o == 0 => Position.Occupied
-            case (Position.Occupied, o) if o >= 4 => Position.Empty
+            case (Position.Occupied, o) if o >= threshold => Position.Empty
             case (p, _) => p
           }
         }
@@ -53,12 +49,37 @@ object Day11 extends App {
       }
       Ferry(state)
     }
-  }
 
-  def constantPoint(init: Ferry): Ferry = {
+    final val directions: Seq[(Int, Int)] = for {
+      dx <- Seq(-1, 0, 1)
+      dy <- Seq(-1, 0, 1)
+      if dx != 0 || dy != 0
+    } yield (dx, dy)
+
+    def adjacentIdxs(f: Ferry, x: Int, y: Int): Seq[(Int, Int)] = directions.map { case (dx, dy) =>
+      (x + dx, y + dy)
+    }
+
+    def visibleIdx(f: Ferry, x: Int, y: Int, dx: Int, dy: Int): Option[(Int, Int)] = {
+      var (nx, ny) = (x + dx, y + dy)
+      while (f.valid(nx, ny)) {
+        val p = f(nx, ny)
+        if (p != Position.Floor) return Some((nx, ny))
+        nx += dx
+        ny += dy
+      }
+      None
+    }
+
+    def visibleIdxs(f: Ferry, x: Int, y: Int): Seq[(Int, Int)] = directions.flatMap { case (dx, dy) =>
+      visibleIdx(f, x, y, dx, dy)
+    }
+   }
+
+  def constantPoint(init: Ferry, adjFunc: AdjFunc, threshold: Int): Ferry = {
     var f = init
     while (true) {
-      val next = f.next
+      val next = f.next(adjFunc, threshold)
       if (next == f) {
         return f
       }
@@ -68,5 +89,6 @@ object Day11 extends App {
   }
 
   val init = Ferry(args.head)
-  println(constantPoint(init).allOccupied)
+//  println(constantPoint(init, Ferry.adjacentIdxs, 4).allOccupied)
+  println(constantPoint(init, Ferry.visibleIdxs, 5).allOccupied)
 }
