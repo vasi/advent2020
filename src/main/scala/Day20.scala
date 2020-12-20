f8e090cf3d070bf802c40aef495e9ed272af37b4
Dave Vasilevsky <dave@vasilevsky.ca>
2020-12-20 01:04:12 -0500

    day20-1

--- src/main/scala/Day20.scala
+++ src/main/scala/Day20.scala
@@ -14,7 +14,7 @@ object Day20 extends App {
   final val HeaderRe = """\ATile (\d+):\z""".r
   def parse(file: String): Seq[Tile] = {
     val tiles = ArrayBuffer.empty[Tile]
-    var grid = ArrayBuffer.empty[String]
+    val grid = ArrayBuffer.empty[String]
     var id = 0
 
     Source.fromFile(file).getLines.foreach {
@@ -30,6 +30,32 @@ object Day20 extends App {
     tiles.toSeq
   }
 
+  // Map a side to the tile ids that contain it
+  type SideMap = Map[String, Seq[Int]]
+  def sideMap(tiles: Seq[Tile]): SideMap = {
+    val sides = tiles.flatMap { tile =>
+      tile.sides.flatMap { side =>
+        Seq(side, side.reverse).map(_ -> tile.id)
+      }
+    }
+    sides.groupBy(_._1).view.mapValues(_.map(_._2)).toMap
+  }
+
+  def connectedSides(tiles: Seq[Tile]): Map[Tile, Int] = {
+    val smap = sideMap(tiles)
+    tiles.map { t =>
+      t -> t.sides.count(s => smap(s).exists(_ != t.id))
+    }.toMap
+  }
+
+  def corners(tiles: Seq[Tile]): Seq[Tile] = {
+    val found = connectedSides(tiles).filter(_._2 == 2)
+    assert(found.size == 4) // could be less if sides are duplicated!
+    found.keys.toSeq
+  }
+
+  def part1(tiles: Seq[Tile]): Long = corners(tiles).map(_.id.toLong).product
+
   val tiles = parse(args.head)
-  println(tiles)
+  println(part1(tiles))
 }
